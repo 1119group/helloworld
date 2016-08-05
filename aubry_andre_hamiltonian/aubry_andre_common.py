ec04463103e49458d04ca66f2dbd099359c66bda
Thomas Look <thomas.look.546@my.csun.edu>
2016-08-04 18:41:14 -0700

    Reformatted aubry_andre_common.py and entropy.py

--- aubry_andre_hamiltonian/aubry_andre_common.py
+++ aubry_andre_hamiltonian/aubry_andre_common.py
@@ -2,19 +2,20 @@ import quantum_module as qm
 from aubry_andre_H import aubry_andre_H
 from aubry_andre_block_H import aubry_andre_block_H
 import numpy as np
-from scipy.sparse import dok_matrix,lil_matrix
+from scipy.sparse import dok_matrix, lil_matrix
 from scipy.sparse.linalg import eigsh
 from scipy.misc import comb
 import sys
- 
-def get_state(Sx,Sy,Sz,N,h,c,phi=0,J=1):
-    s = Sx.get_shape()[0]
-    D = s**N
-    H = aubry_andre_H(Sx,Sy,Sz,N,h,c,phi)
 
-    E_max = eigsh(H,k=1,which='LA',maxiter=1e6,return_eigenvectors=False)
-    E_min = eigsh(H,k=1,which='SA',maxiter=1e6,return_eigenvectors=False)
-    E = np.append(E_min,E_max)
+
+def get_state(Sx, Sy, Sz, N, h, c, phi=0, J=1):
+    s = Sx.get_shape()[0]
+    D = s ** N
+    H = aubry_andre_H(Sx, Sy, Sz, N, h, c, phi)
+
+    E_max = eigsh(H, k=1, which='LA', maxiter=1e6, return_eigenvectors=False)
+    E_min = eigsh(H, k=1, which='SA', maxiter=1e6, return_eigenvectors=False)
+    E = np.append(E_min, E_max)
 
     # Create initial state.
     counter = 0
@@ -22,13 +23,13 @@ def get_state(Sx,Sy,Sz,N,h,c,phi=0,J=1):
     #  a binary number which has an equal number of 1's and 
     #  0's.
     index = np.zeros(N)
-    for k in range(int(round(0.5*N))):
+    for k in range(int(round(0.5 * N))):
         index[k] = 1
 
     exit_status = False
     while True:
         counter += 1
-        zero_Sz_basis_count = int(round(comb(N,0.5*N)))
+        zero_Sz_basis_count = int(round(comb(N, 0.5 * N)))
         index = qm.permute_one_zero(index)
         # Then we convert the binary number into decimal and put a 1
         #  at the spot indicated by the binary into a zero vector. That
@@ -36,53 +37,54 @@ def get_state(Sx,Sy,Sz,N,h,c,phi=0,J=1):
         #  -- zero magnetization.
         s = 0
         for k in range(N):
-            s += int(index[k] * 2**k)
-        psi_0 = dok_matrix((D,1),complex)
-        psi_0[s,0] = 1
+            s += int(index[k] * 2 ** k)
+        psi_0 = dok_matrix((D, 1), complex)
+        psi_0[s, 0] = 1
         # Make sure psi's energy density is very close to 0.5.
         exp_val = psi_0.conjtransp().dot(H.dot(psi_0))
-        e = (exp_val[0,0] - E[0]) / (E[-1] - E[0])
-        if abs(e-0.5) < 0.001:
+        e = (exp_val[0, 0] - E[0]) / (E[-1] - E[0])
+        if abs(e - 0.5) < 0.001:
             break
         # Display an error message when no suitable state is found.
         elif counter > zero_Sz_basis_count:
-           exit_status = True
-           break
+            exit_status = True
+            break
     return H, psi_0, exit_status
 
-def get_state_blk(Sx,Sy,Sz,N,h,c,phi=0,J=1):
+
+def get_state_blk(Sx, Sy, Sz, N, h, c, phi=0, J=1):
     s = Sx.get_shape()[0]
-    D = s**N
+    D = s ** N
     j = 0
     redo = True
     while redo:
-        s = np.random.random_integers(1e9,size=(10,))
-        H = aubry_andre_block_H(Sx,Sy,Sz,N,h,seed=s)
-        
-        E_max,eigvects_max = eigsh(H_norm,k=1,which='LA',maxiter=1e6)
-        E_min,eigvects_min = eigsh(H_norm,k=1,which='SA',maxiter=1e6)
-        E = np.append(E_min,E_max)
-        
+        s = np.random.random_integers(1e9, size=(10,))
+        H = aubry_andre_block_H(Sx, Sy, Sz, N, h, seed=s)
+
+        E_max, eigvects_max = eigsh(H_norm, k=1, which='LA', maxiter=1e6)
+        E_min, eigvects_min = eigsh(H_norm, k=1, which='SA', maxiter=1e6)
+        E = np.append(E_min, E_max)
+
         # Create initial state.
         counter = 0
         while True:
             counter += 1
             # Generate a random state psi with total <Sz> = 0.
-            center_block_size = int(comb(N,N/2))
-            psi = lil_matrix((D,1),dtype=complex)
-            rand_1 = np.random.randint(0,center_block_size)
-            psi[int(0.5*(D-center_block_size))+rand_1,0] = 1
+            center_block_size = int(comb(N, N / 2))
+            psi = lil_matrix((D, 1), dtype=complex)
+            rand_1 = np.random.randint(0, center_block_size)
+            psi[int(0.5 * (D - center_block_size)) + rand_1, 0] = 1
 
             # Make sure psi's energy density is very close to 0.5.
             exp_val = psi.transpose().dot(H_block.dot(psi))
-            e = (np.real(exp_val[0,0]) - E[0]) / (E[-1] - E[0])
-            if abs(e-0.5) < 0.001:
+            e = (np.real(exp_val[0, 0]) - E[0]) / (E[-1] - E[0])
+            if abs(e - 0.5) < 0.001:
                 redo = False
                 j += 1
                 break
             # Regenerate the Hamiltonian after failing to generate any state
             #  for a number of times.
-            elif counter > 2*center_block_size:
+            elif counter > 2 * center_block_size:
                 j += 1
                 break
-    return H_block,psi
+    return H_block, psi
--- aubry_andre_hamiltonian/entropy.py
+++ aubry_andre_hamiltonian/entropy.py
@@ -1,58 +1,66 @@
+from scipy.signal.ltisys import place_poles
+
 import quantum_module as qm
 from aubry_andre_common import get_state
 import numpy as np
-from scipy.sparse.linalg import expm_multiply,expm
+from scipy.sparse.linalg import expm_multiply, expm
 
-def plot_entropy_time_evo_log(spin,N,h,c,phi,time_range_lower_lim,
-                                time_range_upper_lim,sample_size):
-    D = int(2*spin+1)**N
-    Sx,Sy,Sz = qm.init(spin)
+
+def plot_entropy_time_evo_log(spin, N, h, c, phi, time_range_lower_lim,
+                              time_range_upper_lim, sample_size):
+    D = int(2 * spin + 1) ** N
+    Sx, Sy, Sz = qm.init(spin)
     entropy_plot = np.zeros(sample_size)
-    init_delta_t,r = qm.get_init_delta_t(time_range_lower_lim,
-                        time_range_upper_lim,sample_size)
-    H,psi,exit_status = get_state(Sx,Sy,Sz,N,h,c,phi)
-    
+    init_delta_t, r = qm.get_init_delta_t(time_range_lower_lim,
+                                          time_range_upper_lim, sample_size)
+    H, psi, exit_status = get_state(Sx, Sy, Sz, N, h, c, phi)
+
     if not exit_status:
         # Plot the first point which does not require time evolution.
-        entropy_plot[0] += qm.get_vn_entropy(psi,spin,N,mode='eqsplit')
+        entropy_plot[0] += qm.get_vn_entropy(psi, spin, N, mode='eqsplit')
 
         # Plot the second point which requires the first time evolution.
         current_delta_t = init_delta_t
-        psi_time_evolved = expm_multiply(-1j*H*current_delta_t,psi)
+        psi_time_evolved = expm_multiply(-1j * H * current_delta_t, psi)
         entropy_plot[1] += qm.get_vn_entropy(psi_time_evolved,
-                                    spin,N,mode='eqsplit')
+                                             spin, N, mode='eqsplit')
 
         # Plot the rest of the points with time evolution.
-        for plot_point in range(2,sample_size):
-            delta_delta_t = qm.get_delta_delta_t(time_range_lower_lim,plot_point,r)
+        for plot_point in range(2, sample_size):
+            delta_delta_t = qm.get_delta_delta_t(time_range_lower_lim,
+                                                 plot_point, r)
             current_delta_t += delta_delta_t
-            psi_time_evolved = expm_multiply(-1j*H*current_delta_t,
-                                    psi_time_evolved)
+            psi_time_evolved = expm_multiply(-1j * H * current_delta_t,
+                                             psi_time_evolved)
             entropy_plot[plot_point] += qm.get_vn_entropy(psi_time_evolved,
-                                            spin,N,mode='eqsplit')
-    return entropy_plot,exit_status
+                                                          spin, N,
+                                                          mode='eqsplit')
+    return entropy_plot, exit_status
 
-def plot_entropy_time_evo_lin(spin,N,h,c,phi,time_range_lower_lim,
-                                time_range_upper_lim,sample_size):
-    D = int(2*spin+1)**N
-    Sx,Sy,Sz = qm.init(spin)
+
+def plot_entropy_time_evo_lin(spin, N, h, c, phi, time_range_lower_lim,
+                              time_range_upper_lim, sample_size):
+    D = int(2 * spin + 1) ** N
+    Sx, Sy, Sz = qm.init(spin)
     entropy_plot = np.zeros(sample_size)
-    delta_t = (time_range_upper_lim-time_range_lower_lim)/(sample_size-1)
-    H,psi,exit_status = get_state(Sx,Sy,Sz,N,h,c,phi)
+    delta_t = (time_range_upper_lim - time_range_lower_lim) / (sample_size - 1)
+    H, psi, exit_status = get_state(Sx, Sy, Sz, N, h, c, phi)
 
     if not exit_status:
-        U = expm(-1j*H*delta_t)
+        U = expm(-1j * H * delta_t)
 
         # Plot the first point which does not require time evolution.
-        entropy_plot[0] += qm.get_vn_entropy(psi,spin,N,mode='eqsplit')
+        entropy_plot[0] += qm.get_vn_entropy(psi, spin, N, mode='eqsplit')
 
         psi_time_evolved = psi
         # Plot the rest of the points.
-        for plot_point in range(1,sample_size):
-            psi_time_evolved = U*psi_time_evolved
+        for plot_point in range(1, sample_size):
+            psi_time_evolved = U * psi_time_evolved
             entropy_plot[plot_point] = qm.get_vn_entropy(psi_time_evolved,
-                                            spin,N,mode='eqsplit')
-    return entropy_plot,exit_status
+                                                         spin, N,
+                                                         mode='eqsplit')
+    return entropy_plot, exit_status
 
-def plot_entropy_var_h(spin,N,h_lower_lim,h_upper_lim,c,phi,sample_size):
-    h_list = np.linspace(h_lower_lim,h_upper_lim,sample_size)
+
+def plot_entropy_var_h(spin, N, h_lower_lim, h_upper_lim, c, phi, sample_size):
+    h_list = np.linspace(h_lower_lim, h_upper_lim, sample_size)
