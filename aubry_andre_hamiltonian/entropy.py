97a4044001aaf3f3d0a3e889ed071e007fe3b4e7
macthecadillac <macthecadillac@gmail.com>
2016-08-08 19:30:21 -0700

    Modularized functions

--- aubry_andre_hamiltonian/entropy.py
+++ aubry_andre_hamiltonian/entropy.py
@@ -1,39 +1,66 @@
 import quantum_module as qm
-from aubry_andre_block_H import aubry_andre_H_off_diag, aubry_andre_H_diag
-from aubry_andre_common import get_state_blk
+import aubry_andre_block_H as aubryH
+from aubry_andre_common import get_0_state_blk
 import numpy as np
 from scipy import io
+from scipy.misc import comb
+from scipy.sparse import lil_matrix
 from scipy.sparse.linalg import expm_multiply, expm
 import os
 
 
-def plot_entropy_time_evo_log(spin, N, h, c, phi, time_range_lower_lim,
-                              time_range_upper_lim, sample_size):
-    Sx, Sy, Sz = qm.init(spin)
-    entropy_plot = np.zeros(sample_size)
-    init_delta_t, r = qm.get_init_delta_t(time_range_lower_lim,
-                                          time_range_upper_lim, sample_size)
+def spin2z(D, N, psi):
+    """
+    Rewrites a given state psi from the spin basis (the basis of
+    the block diagonalized Hamiltonian) to the conventional
+    Sz product basis.
+    """
+    psi_tz = lil_matrix((D, 1), dtype=complex)
+    j_max = int(round(0.5 * N))
+    blk_sz = int(round(comb(N, j_max)))
+    basis_set_0, basis_dict_0 = aubryH.basis_set(N, blk_sz, j_max, 0)
+    for i in basis_dict_0:
+        psi_tz[i] = psi[basis_dict_0[i], 0]
 
-    # Check and see if the off diagonal matrix is saved. If so, load from disk.
+    psi_tz = psi_tz.tocsc()
+    return psi_tz
+
+
+def generate_H(Sx, Sy, Sz, N, h, c, phi):
+    # Check and see if the off diagonal matrix is saved.
+    #  If so, load from disk.
     if os.path.isfile('block_H_off_diag_' + str(N) + 'spins.mat'):
         H_off_diag = io.loadmat('block_H_off_diag_' +
                                 str(N) + 'spins.mat')['i']
     else:
-        H_off_diag = aubry_andre_H_off_diag(Sx, Sy, Sz, N)
+        H_off_diag = aubryH.aubry_andre_H_off_diag(Sx, Sy, Sz, N)
         H_off_diag += H_off_diag.transpose()
         io.savemat('block_H_off_diag_' + str(N) + 'spins', {'i': H_off_diag})
-    H_diag = aubry_andre_H_diag(Sx, Sy, Sz, N, h, c, phi)
+    H_diag = aubryH.aubry_andre_H_diag(Sx, Sy, Sz, N, h, c, phi)
     H = H_diag + H_off_diag
-    H = H.tolil()
+    return H
 
-    psi, exit_status = get_state_blk(H, N)
-    if not exit_status:
+
+def plot_entropy_time_evo_log(spin, N, h, c, phi, time_range_lower_lim,
+                              time_range_upper_lim, sample_size):
+    D = int(2 * spin + 1) ** N
+    Sx, Sy, Sz = qm.init(spin)
+    entropy_plot = np.zeros(sample_size)
+    init_delta_t, r = qm.get_init_delta_t(time_range_lower_lim,
+                                          time_range_upper_lim, sample_size)
+    H = generate_H(Sx, Sy, Sz, N, h, c, phi)
+    psi, error = get_0_state_blk(H, N)
+
+    # Rewrite psi in the Sz basis
+    psi_tz = spin2z(D, N, psi)
+
+    if not error:
         # Plot the first point which does not require time evolution.
-        entropy_plot[0] += qm.get_vn_entropy(psi, spin, N, mode='eqsplit')
+        entropy_plot[0] += qm.get_vn_entropy(psi_tz, spin, N, mode='eqsplit')
 
         # Plot the second point which requires the first time evolution.
         current_delta_t = init_delta_t
-        psi_time_evolved = expm_multiply(-1j*H*current_delta_t, psi)
+        psi_time_evolved = expm_multiply(-1j*H*current_delta_t, psi_tz)
         entropy_plot[1] += qm.get_vn_entropy(psi_time_evolved,
                                              spin, N, mode='eqsplit')
 
@@ -47,54 +74,47 @@ def plot_entropy_time_evo_log(spin, N, h, c, phi, time_range_lower_lim,
             entropy_plot[plot_point] += qm.get_vn_entropy(psi_time_evolved,
                                                           spin, N,
                                                           mode='eqsplit')
-    return entropy_plot, exit_status
+    return entropy_plot, error
 
 
 def plot_entropy_time_evo_lin(spin, N, h, c, phi, time_range_lower_lim,
                               time_range_upper_lim, sample_size):
+    D = int(2 * spin + 1) ** N
     Sx, Sy, Sz = qm.init(spin)
     entropy_plot = np.zeros(sample_size)
     delta_t = (time_range_upper_lim-time_range_lower_lim)/(sample_size-1)
+    H = generate_H(Sx, Sy, Sz, N, h, c, phi)
+    psi, error = get_0_state_blk(H, N)
 
-    # Check and see if the off diagonal matrix is saved. If so, load from disk.
-    if os.path.isfile('block_H_off_diag_' + str(N) + 'spins.mat'):
-        H_off_diag = io.loadmat('block_H_off_diag_' +
-                                str(N) + 'spins.mat')['i']
-    else:
-        H_off_diag = aubry_andre_H_off_diag(Sx, Sy, Sz, N)
-        H_off_diag += H_off_diag.transpose()
-        io.savemat('block_H_off_diag_' + str(N) + 'spins', {'i': H_off_diag})
-    H_diag = aubry_andre_H_diag(Sx, Sy, Sz, N, h, c, phi)
-    H = H_diag + H_off_diag
+    # Rewrite psi in the Sz basis
+    psi_tz = spin2z(D, N, psi)
 
-    psi, exit_status = get_state_blk(H, N)
-    if not exit_status:
+    if not error:
         U = expm(-1j*H*delta_t)
 
         # Plot the first point which does not require time evolution.
-        entropy_plot[0] += qm.get_vn_entropy(psi, spin, N, mode='eqsplit')
+        entropy_plot[0] += qm.get_vn_entropy(psi_tz, spin, N, mode='eqsplit')
 
-        psi_time_evolved = psi
+        psi_time_evolved = psi_tz
         # Plot the rest of the points.
         for plot_point in range(1, sample_size):
             psi_time_evolved = U*psi_time_evolved
             entropy_plot[plot_point] = qm.get_vn_entropy(psi_time_evolved,
                                                          spin, N,
                                                          mode='eqsplit')
-    return entropy_plot, exit_status
+    return entropy_plot, error
 
 
 def plot_entropy_var_h(spin, N, h_lower_lim, h_upper_lim, c, phi, sample_size):
-    #This is just a rough outline...
+    # FIXME: This is just a rough outline...
     h_list = np.linspace(h_lower_lim, h_upper_lim, sample_size)
     D = int(2 * spin + 1) ** N
     Sx, Sy, Sz = qm.init(spin)
     entropy_plot = np.zeros(sample_size)
 
     for h in h_list:
-        psilist, exit_status = get_0_states(Sx, Sy, Sz, N, h, c, phi)
+        psilist, error = get_0_states(Sx, Sy, Sz, N, h, c, phi)
         for psi in psilist:
             entropy_plot[h] += qm.get_vn_entropy(psi, spin, N, mode='eqsplit')
         entropy_plot[h] /= len(psilist)
-    return entropy_plot, exit_status
-
+    return entropy_plot, error
