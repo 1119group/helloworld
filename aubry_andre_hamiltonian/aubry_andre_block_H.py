2698d5940be73a84900744228623f00cfeb91975
macthecadillac <macthecadillac@gmail.com>
2016-08-09 22:49:31 -0700

    More refactoring
    Added the ability to save all all the basis state binary representations
    to disk.
    Added the ability to save every single off diagonal block matrix for
    each total Sz.
    Added a function to compute one specifc block of the Hamiltonian.
    Added a function to compute the entire block diagonalized Hamiltonian.

--- aubry_andre_hamiltonian/aubry_andre_block_H.py
+++ aubry_andre_hamiltonian/aubry_andre_block_H.py
@@ -5,8 +5,11 @@ fermionic Aubry-Andre Hamiltonian.
 
 import quantum_module as qm
 import numpy as np
+from scipy import io
 from scipy.sparse import lil_matrix
 from scipy.misc import comb
+import os
+import pickle
 
 
 def bin2dec(l):
@@ -29,31 +32,66 @@ def basis_set(N, blk_sz, j_max, current_j):
     are seeking the basis set for.
     "current_j" is the total <Sz> for the current block.
     """
-    # Find all the binary representations of the current j.
-    D = 2**N
-    basis_set_seed = [0] * N
-    basis_ind_dict = {}
-    basis_index = 0
-    for n in range(j_max + current_j):
-        basis_set_seed[N - 1 - n] = 1
-    current_j_basis_set = []
-    if blk_sz != 1:
-        for i in range(blk_sz):
-            current_j_basis_set.append(qm.permute_one_zero(
-                                       basis_set_seed)[:])
-            # Create a dictionary that links the decimal
-            #  representation of a basis and its position in this
-            #  particular way of basis ordering.
-            basis_ind_dict[D - 1 - bin2dec(basis_set_seed)] = basis_index
-            basis_index += 1
+    bs_fname = 'cache/basisset_' + str(N) + 'spins_' + str(current_j) + 'Sz'
+    bidict_fname = 'cache/basisinddict_' + str(N)
+    bidict_fname += 'spins_' + str(current_j) + 'Sz'
+    if os.path.isfile(bs_fname) and os.path.isfile(bidict_fname):
+        data0 = open(bs_fname, 'rb')
+        data1 = open(bidict_fname, 'rb')
+        current_j_basis_set = pickle.load(data0)
+        basis_ind_dict = pickle.load(data1)
+        data0.close()
+        data1.close()
     else:
-        # The permute function cannot permute lists for which there is
-        #  only one permutation.
-        current_j_basis_set.append(basis_set_seed)
+        # Find all the binary representations of the current j.
+        D = 2**N
+        basis_set_seed = [0] * N
+        basis_ind_dict = {}
+        basis_index = 0
+        for n in range(j_max + current_j):
+            basis_set_seed[N - 1 - n] = 1
+        current_j_basis_set = []
+        if blk_sz != 1:
+            for i in range(blk_sz):
+                current_j_basis_set.append(qm.permute_one_zero(
+                                           basis_set_seed)[:])
+                # Create a dictionary that links the decimal
+                #  representation of a basis and its position in this
+                #  particular way of basis ordering.
+                basis_ind_dict[D - 1 - bin2dec(basis_set_seed)] = basis_index
+                basis_index += 1
+        else:
+            # The permute function cannot permute lists for which there is
+            #  only one permutation.
+            current_j_basis_set.append(basis_set_seed)
+        with open(bs_fname, 'wb') as data0:
+            pickle.dump(current_j_basis_set, data0, pickle.HIGHEST_PROTOCOL)
+        with open(bidict_fname, 'wb') as data1:
+            pickle.dump(basis_ind_dict, data1, pickle.HIGHEST_PROTOCOL)
     return current_j_basis_set, basis_ind_dict
 
 
-def blk_off_diag(Sx, Sy, Sz, N, total_Sz, J=1):
+def iowrapper(func, fname, options):
+    """
+    Wrapper for I/O for smoother operations. Works only with functions
+    that produce triangular sparse matrices which could be save with the
+    scipy.savemat function.
+    "func" is the function to be wrapped.
+    "fname" is the name of the file to be saved/loaded.
+    "options" is a list of parameters to be passed on to the function.
+    """
+    # Check and see if the off diagonal matrix is saved.
+    #  If so, load from disk.
+    if os.path.isfile(fname):
+        operator = io.loadmat(fname)['i']
+    else:
+        operator = func(options)
+        operator += operator.transpose()
+        io.savemat(fname, {'i': operator}, appendmat=False)
+    return operator
+
+
+def blk_off_diag_nocache(options):
     """
     Privides the upper half of the off_diagonal elements of one
     block of the Hamiltonian corresponding to the given total Sz.
@@ -61,7 +99,9 @@ def blk_off_diag(Sx, Sy, Sz, N, total_Sz, J=1):
     "total_Sz" is the total spin of all the basis states in the current block.
     "pos" is the i or j coordinates of the upper left element of
     the current block in the full Hamiltonian.
+    This function does not save the results on disk.
     """
+    [Sx, Sy, Sz, N, total_Sz, J] = options
     D = Sx.get_shape()[0]**N
     # Side length of the current block.
     j_max = int(round(0.5 * N))
@@ -88,6 +128,21 @@ def blk_off_diag(Sx, Sy, Sz, N, total_Sz, J=1):
             k -= 1
         for k in nz_list:
             H_curr_blk_off_diag[i, k] = 0.5 * J
+    return H_curr_blk_off_diag
+
+
+def blk_off_diag(Sx, Sy, Sz, N, total_Sz, J=1):
+    """
+    Privides the off_diagonal elements of one block of the Hamiltonian
+    corresponding to the given total Sz.
+    "total_Sz" is the total spin of all the basis states in the current block.
+    "pos" is the i or j coordinates of the upper left element of
+    the current block in the full Hamiltonian.
+    """
+    options = [Sx, Sy, Sz, N, total_Sz, J]
+    fname = 'cache/block_H_off_diag_' + str(N) + 'spins_'
+    fname += str(total_Sz) + 'Sz.mat'
+    H_curr_blk_off_diag = iowrapper(blk_off_diag_nocache, fname, options)
     return H_curr_blk_off_diag
 
 
@@ -130,10 +185,16 @@ def blk_diag(Sx, Sy, Sz, N, h, c, total_Sz, phi=0):
     return H_curr_blk_diag
 
 
+def blk_full(Sx, Sy, Sz, N, h, c, total_Sz, phi=0, J=1):
+    """Generates block of the Hamiltonian for a specific total <Sz>."""
+    H_curr_blk_diag = blk_diag(Sx, Sy, Sz, N, h, c, total_Sz, phi)
+    H_curr_blk_off_diag = blk_off_diag(Sx, Sy, Sz, N, total_Sz, J)
+    return H_curr_blk_diag + H_curr_blk_off_diag
+
+
 def aubry_andre_H_off_diag(Sx, Sy, Sz, N, J=1):
     """
-    Provides the upper half of the off-diagonal elements of the
-    Hamiltonian. Returns an upper triangle sparse matrix.
+    Provides the off-diagonal elements of the Hamiltonian.
     """
     D = Sx.get_shape()[0]**N
     H_off_diag = lil_matrix((D, D), dtype=complex)
@@ -166,3 +227,14 @@ def aubry_andre_H_diag(Sx, Sy, Sz, N, h, c, phi=0):
         pos += blk_sz
         current_j -= 1
     return H_diag
+
+
+def aubry_andre_H(Sx, Sy, Sz, N, h, c, phi):
+    """
+    A function to put together the pieces of a Hamiltonian and
+    generate a full Hamiltonian.
+    """
+    H_off_diag = aubry_andre_H_off_diag(Sx, Sy, Sz, N)
+    H_diag = aubry_andre_H_diag(Sx, Sy, Sz, N, h, c, phi)
+    H = H_diag + H_off_diag
+    return H
