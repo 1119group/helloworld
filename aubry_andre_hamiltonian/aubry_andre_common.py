90e5e28ba1ab6e054962bbb741e96f004ed485db
macthecadillac <macthecadillac@gmail.com>
2016-08-09 22:53:58 -0700

    Added some functions and applied some fixes
    Updated the spin2z function to give it a significant performance boost.
    Added a new function "recast" to take a state that only contains element
    of a certain total Sz and augment it to include all other elements.
    Rewrite energy_density to use a function from quantum_module.
    Added a function to find a state with only the center block (zero total
    Sz) of a Hamiltonian.

--- aubry_andre_hamiltonian/aubry_andre_common.py
+++ aubry_andre_hamiltonian/aubry_andre_common.py
@@ -5,16 +5,15 @@ import numpy as np
 from scipy.sparse import dok_matrix, lil_matrix
 from scipy.sparse.linalg import eigsh
 from scipy.misc import comb
-from scipy import io
-import os
 
 
-def spin2z(D, N, psi):
+def spin2z_old(D, N, psi):
     """
     Rewrites a given state psi from the spin basis (the basis of
     the block diagonalized Hamiltonian) to the conventional
     Sz product basis.
     Only works for zero total <Sz>.
+    Slow.
     """
     psi_tz = lil_matrix((D, 1), dtype=complex)
     j_max = int(round(0.5 * N))
@@ -25,6 +24,26 @@ def spin2z(D, N, psi):
         psi_tz[i, 0] = psi[basis_dict_0[i] + blk_pos, 0]
 
     psi_tz = psi_tz.tocsc()
+    return psi_tz
+
+
+def spin2z(D, N, psi):
+    """
+    Rewrite a given state psi from the spin basis (the basis of
+    the block diagonalized Hamiltonian) to the conventional
+    Sz product basis.
+    Much faster than the other verions.
+    """
+    psi_tz = lil_matrix((D, 1), dtype=complex)
+    j_max = int(round(0.5 * N))
+    blk_sz = int(round(comb(N, j_max)))
+    basis_set_0, basis_dict_0 = aubryH.basis_set(N, blk_sz, j_max, 0)
+    shift = int(round(0.5 * (D - blk_sz)))
+    for i in psi.nonzero()[0]:
+        if i >= shift and i < D - shift:
+            l = basis_set_0[i - shift]
+            dec = aubryH.bin2dec(l)
+            psi_tz[D - 1 - dec, 0] = psi[i, 0]
     return psi_tz
 
 
@@ -54,23 +73,32 @@ def generate_H(Sx, Sy, Sz, N, h, c, phi):
     A function to put together the pieces of a Hamiltonian and
     generate a full Hamiltonian.
     """
-    # Check and see if the off diagonal matrix is saved.
-    #  If so, load from disk.
-    if os.path.isfile('block_H_off_diag_' + str(N) + 'spins.mat'):
-        H_off_diag = io.loadmat('block_H_off_diag_' +
-                                str(N) + 'spins.mat')['i']
-    else:
-        H_off_diag = aubryH.aubry_andre_H_off_diag(Sx, Sy, Sz, N)
-        H_off_diag += H_off_diag.transpose()
-        io.savemat('block_H_off_diag_' + str(N) + 'spins', {'i': H_off_diag})
+    H_off_diag = aubryH.aubry_andre_H_off_diag(Sx, Sy, Sz, N)
     H_diag = aubryH.aubry_andre_H_diag(Sx, Sy, Sz, N, h, c, phi)
     H = H_diag + H_off_diag
     return H
 
 
+def recast(N, psi_short):
+    """
+    This function takes in a state psi which contains only the elements
+    corresponding to a zero total <Sz> and augment it into one that
+    would contain elements of all other total <Sz>'s.
+    This by no means changes the basis into a Sz product basis. For that
+    function please refer to spin2z.
+    """
+    D = 2 ** N
+    j_max = int(round(0.5 * N))
+    blk_sz = int(round(comb(N, j_max)))
+    shift = int(round(0.5 * (D - blk_sz)))
+    psi_long = lil_matrix((D, 1), dtype=complex)
+    psi_long[shift:D - shift, 0] = psi_short[:, :]
+    return psi_long
+
+
 def energy_density(psi, H, E):
-    exp_val = psi.conjtransp().dot(H.dot(psi))
-    e = (exp_val[0, 0] - E[0]) / (E[-1] - E[0])
+    ev = qm.exp_value(H, psi)
+    e = (ev - E[0]) / (E[-1] - E[0])
     return e
 
 
@@ -97,7 +125,10 @@ def spin_basis(N, counter):
 
 
 def spin_basis_0(N, counter):
-    """Returns a state psi in the spin basis."""
+    """
+    Returns a state psi in the spin basis. The vector only contains
+    elements corresponding to a zero total <Sz>.
+    """
     zero_Sz_basis_count = int(round(comb(N, 0.5 * N)))
     psi_0 = dok_matrix((zero_Sz_basis_count, 1), dtype=complex)
     psi_0[counter, 0] = 1
@@ -141,16 +172,27 @@ def init_state(N, H, basis):
 
 
 def get_state(Sx, Sy, Sz, N, h, c, phi=0, J=1):
+    """Works for the Sz product basis Hamiltonian."""
     H = aubry_andre_H(Sx, Sy, Sz, N, h, c, phi)
     H, psi_0, error = init_state(N, H, z_basis)
     return H, psi_0, error
 
 
-def get_0_state_blk(H, N):
+def get_state_blk_full(H, N):
     """
-    "H" is only the center block of the Hamiltonian.
+    "H" is the full block diagonalized operator. Works for the spin_basis
+    Hamiltonian.
     """
     H, psi, error = init_state(N, H, spin_basis)
+    return psi, error
+
+
+def get_state_blk(H, N):
+    """
+    "H" is only the center block of the block diagonalized Hamiltonian.
+    Works for the spin_basis Hamiltonian.
+    """
+    H, psi, error = init_state(N, H, spin_basis_0)
     return psi, error
 
 
@@ -165,8 +207,8 @@ def average_adj_gap_ratio(sorted_eigenvalue_list):
     adj_gap_ratio = 0
     delta_n_list = np.diff(sorted_eigenvalue_list)
     for x in range(0, len(delta_n_list) - 1):
-        adj_gap_ratio += min(delta_n_list[x],delta_n_list[x + 1]) /\
-                         max(delta_n_list[x],delta_n_list[x + 1])
+        adj_gap_ratio += min(delta_n_list[x], delta_n_list[x + 1]) /\
+            max(delta_n_list[x], delta_n_list[x + 1])
     adj_gap_ratio /= len(delta_n_list) - 1
     return adj_gap_ratio
 
