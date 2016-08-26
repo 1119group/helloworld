1751d5b01a92a58e1c7b587026a507f12785b1ec
Mac Lee <macthecadillac@gmail.com>
2016-08-26 13:20:56 -0700

    Fixed some bugs

--- aubry_andre_hamiltonian/temporal_correlation.py
+++ aubry_andre_hamiltonian/temporal_correlation.py
@@ -13,7 +13,10 @@ def get_ground_state(N, h, c, phi):
     H_blks = []
     j_max = int(round(0.5 * N))
     # Insert the eigenenergy and eigenstate for very first "block" of H
-    V_first = ss.lil_matrix((2 ** N, 1))
+    H_first = aubh.blk_diag(N, h, c, j_max, phi)
+    H_blks.append(H_first.copy())
+    E.append(H_first[0, 0])
+    V_first = ss.lil_matrix((1, 1))
     V_first[0, 0] = 1
     V.append(V_first.tocsc())
 
@@ -22,37 +25,39 @@ def get_ground_state(N, h, c, phi):
         H_blks.append(H_j.copy())
 
         E_j_0, V_j_0 = ss.linalg.eigsh(H_j, k=1, which='SA', maxiter=1e6)
-        E.append(E_j_0)
+        E.append(E_j_0[0])
         V.append(ss.csc_matrix(V_j_0))
-        E_j_1, V_j_1 = ss.linalg.eigsh(H_j, k=1, which='LA', maxiter=1e6)
-        E.append(E_j_1)
-        V.append(ss.csc_matrix(V_j_1))
 
     # Insert the eigenenergy and eigenstate for very last "block" of H
-    E.append(aubh.blk_diag(N, h, c, -1 * j_max, phi))
-    V_last = ss.lil_matrix((2 ** N, 1))
-    V_last[-1, 0] = 1
+    H_last = aubh.blk_diag(N, h, c, -1 * j_max, phi)
+    H_blks.append(H_last.copy())
+    E.append(H_last[0, 0])
+    V_last = ss.lil_matrix((1, 1))
+    V_last[0, 0] = 1
     V.append(V_last.tocsc())
-    E_sorted, V_sorted = qm.sort_eigs(E, V)
-    j_rev = (E.index(E_sorted[0]) - 1) // 2
-    return H_blks[j_rev], E_sorted[0], V_sorted[0], j_max - j_rev
+
+    E_min = sorted(E)[0]
+    j_rev = E.index(E_min)
+    psi_0 = V[j_rev]
+    total_Sz = N + 1 - j_rev - j_max
+    return H_blks[j_rev], E_min, psi_0, total_Sz
 
 
 def temporal_correlation(psi_0, U_t, t, S_k, E_0):
     psia = S_k * psi_0
     temp_corr = np.exp(1j * E_0 * t) * psia.transpose().conjugate() * U_t * psia
-    # temp_corr = np.exp(1j * E_0 * t) * psi_0.transpose().conjugate()
-    # temp_corr *= U_t
-    # temp_corr *= S_k * psi_0
-    temp_corr = np.real(temp_corr)
-    return temp_corr
+    temp_corr = np.real(temp_corr[0, 0])
+    return float(temp_corr)
 
 
 def plot_linear_time_evolution(N, h, c, phi, k, start_time, end_time, n_points):
     timer = Timer(n_points, jname='temporal correlation:')
     plot = []
     H, E_0, psi_0, total_Sz = get_ground_state(N, h, c, phi)
+    H = H.tocsc()
+    psi_0 = psi_0.tocsc()
     blk_sz = H.get_shape()[0]
+
     j_max = int(round(0.5 * N))
     n = j_max + total_Sz
     shift = 0
@@ -61,10 +66,8 @@ def plot_linear_time_evolution(N, h, c, phi, k, start_time, end_time, n_points):
 
     Sx, Sy, Sz = qm.init(0.5)
     S_k = qm.get_full_matrix(Sz, k, N)
-    S_k = aubc.Sz2spin_basis(N, S_k)
-    S_k = S_k[shift:shift + blk_sz, shift:shift + blk_sz]
-
-    print('\ntotal_Sz:', total_Sz, 'psi_0:', psi_0.get_shape(), 'H:', H.get_shape(), 'S_k:', S_k.get_shape())
+    S_k = aubc.Sz2spin_basis(N, S_k).tolil()
+    S_k = S_k[shift:shift + blk_sz, shift:shift + blk_sz].tocsc()
 
     # Evolve the unitary operator to start_time and plot the first point.
     U_t = ss.linalg.expm(-1j * H * start_time)
@@ -73,9 +76,10 @@ def plot_linear_time_evolution(N, h, c, phi, k, start_time, end_time, n_points):
     timer.progress()
 
     dt = (end_time - start_time) / (n_points - 1)
-    U_t = ss.linalg.expm(-1j * H * dt)
+    U_t_const = ss.linalg.expm(-1j * H * dt)
     t = np.linspace(start_time, end_time, n_points)
     for i in range(1, n_points):
+        U_t = U_t_const * U_t
         temp_corr = temporal_correlation(psi_0, U_t, t[i], S_k, E_0)
         plot.append(temp_corr)
         timer.progress()
