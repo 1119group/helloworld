2fedbf6d94db0af8be57859a1be736c65dc5032d
Mac Lee <macthecadillac@gmail.com>
2016-10-20 16:24:59 -0700

    Added a function for a full block. Simplified some code.

--- aubry_andre_hamiltonian/aubry_andre_block_twoleg_H.py
+++ aubry_andre_hamiltonian/aubry_andre_block_twoleg_H.py
@@ -11,7 +11,11 @@ import scipy.sparse as sp
 from scipy.misc import comb
 
 
-def bin_to_dec(l):
+def _abs_diff(x, y):
+    return abs(x - y)
+
+
+def _bin_to_dec(l):
     """Converts a list "l" of 1s and 0s into a decimal"""
     return int(''.join(map(str, l)), 2)
 
@@ -33,7 +37,7 @@ def create_complete_basis(N, current_j):
         except IndexError:                # When current_j is N // 2 or -N // 2
             pass
         basis_set.append(basis[:])
-        decimal_basis = bin_to_dec(basis)
+        decimal_basis = _bin_to_dec(basis)
         to_diag[decimal_basis] = i      # i is the index within only this block
         to_ord[i] = dim - decimal_basis - 1
     return basis_set, to_diag, to_ord
@@ -41,7 +45,7 @@ def create_complete_basis(N, current_j):
 
 def diagonal_single_block(N, h, c, phi, J1, J2, I, current_j):
     """
-    Creates the diagonal of the Hamiltonian.
+    Creates the diagonal of a block of the Hamiltonian.
 
     Args: 'N' System length
           'h' Field strength
@@ -55,7 +59,7 @@ def diagonal_single_block(N, h, c, phi, J1, J2, I, current_j):
     """
     basis_set = create_complete_basis(N, current_j)[0]
     blksize = len(basis_set)
-    diag = np.zeros(blksize)
+    diagonal = np.zeros(blksize)
     sites = np.array(range(1, N // I + 1)).repeat(I, axis=0)
     field = h * np.cos(2 * np.pi * c * sites + phi)
     d = {1: 1, 0: -1}         # For conversion of 0s to -1s in the basis later
@@ -64,27 +68,27 @@ def diagonal_single_block(N, h, c, phi, J1, J2, I, current_j):
         # Number of repeated 1s and 0s separated by I
         #  Compute absolute values of differences of pairs separated by I
         #  and sum. Periodic BC (horizontal interaction).
-        diff_pairs = sum(map(lambda x, y: abs(x - y), b, b[I:] + b[:I]))
+        diff_pairs = sum(map(_abs_diff, b, b[I:] + b[:I]))
         same_pairs = N - diff_pairs
-        diag[i] += 0.25 * J1 * (same_pairs - diff_pairs)
+        diagonal[i] += 0.25 * J1 * (same_pairs - diff_pairs)
 
         # Number of different adjacent 1s and 0s.
         #  Closed BC (interaction between legs)
         if not I <= 1:
             comp = [m for m in range(N) if not (m + 1) % I == 0]
-            diff_pairs = sum(map(lambda x, y: abs(x - y),
-                             [b[m] for m in comp], [b[m + 1] for m in comp]))
+            diff_pairs = sum(map(_abs_diff, [b[m] for m in comp],
+                                 [b[m + 1] for m in comp]))
             same_pairs = len(comp) - diff_pairs
-            diag[i] += 0.25 * J2 * (same_pairs - diff_pairs)
+            diagonal[i] += 0.25 * J2 * (same_pairs - diff_pairs)
 
         # ***Field Terms***
-        diag[i] += 0.5 * sum([d[m] for m in b] * field)
-    return sp.diags(diag, 0, dtype=complex)
+        diagonal[i] += 0.5 * sum([d[m] for m in b] * field)
+    return sp.diags(diagonal, 0, dtype=complex)
 
 
 def off_diagonal_single_block(N, J1, J2, I, current_j):
     """
-    Creates the off diagonals of the Hamiltonian.
+    Creates the off diagonals of a block of the Hamiltonian.
 
     Args: 'N' System size
           'J1' Coupling constant between neighboring sites
@@ -97,18 +101,18 @@ def off_diagonal_single_block(N, J1, J2, I, current_j):
         """Sets non-zero elements in the matrix"""
         bj = bi[:]
         bj[pair[0]], bj[pair[1]] = bj[pair[1]], bj[pair[0]]
-        if not sum(map(lambda x, y: abs(x - y), bi, bj)) == 0:
-            j = to_diag[bin_to_dec(bj)]
-            off_diag[i, j] = 0.5 * J
+        if not sum(map(_abs_diff, bi, bj)) == 0:
+            j = to_diag[_bin_to_dec(bj)]
+            off_diagonal[i, j] = 0.5 * J
 
     basis_set, to_diag, to_ord = create_complete_basis(N, current_j)
     blksize = len(basis_set)
-    off_diag = sp.lil_matrix((blksize, blksize), dtype=complex)
+    off_diagonal = sp.lil_matrix((blksize, blksize), dtype=complex)
 
     # Pairs of elements to inspect. I_pairs = pairs I sites apart
-    #  adj_pairs = pairs adjacent to each other
+    #  adjacent_pairs = pairs adjacent to each other
     I_pairs = [(N + i if i < 0 else i, i + I) for i in range(-I, N - I)]
-    adj_pairs = [(i, i + 1) for i in range(N - 1) if not (i + 1) % I == 0]
+    adjacent_pairs = [(i, i + 1) for i in range(N - 1) if not (i + 1) % I == 0]
 
     for i, bi in enumerate(basis_set):
         # Flipping of elements I sites apart
@@ -117,6 +121,25 @@ def off_diagonal_single_block(N, J1, J2, I, current_j):
 
         # Flipping of elements adjacent to each other
         if not I <= 1:
-            for pair in adj_pairs:
+            for pair in adjacent_pairs:
                 non_zero_element(i, bi, pair, J2)
-    return off_diag
+    return off_diagonal
+
+
+def single_block(N, h, c, phi, J1=1, J2=1, I=2, current_j=0):
+    """
+    Creates a block of the Hamiltonian
+
+    Args: 'N' System length
+          'h' Field strength
+          'c' Irrational/transcendental number happens to be in the field
+          'phi' Phase shift
+          'J1' Interaction constant between sites along a leg
+          'J2' Interaction constant between legs
+          'I' Number of legs
+          'current_j' Total <Sz>
+    Returns: Sparse matrix (CSC matrix)
+    """
+    diagonals = diagonal_single_block(N, h, c, phi, J1, J2, I, current_j)
+    off_diagonals = off_diagonal_single_block(N, J1, J2, I, current_j)
+    return diagonals + off_diagonals
